60c13776ff04a25bcb020b6646a8917d0ded0732
Plasma0 <konrad_kania@wp.pl>
2017-05-17 15:04:02 +0200

    dodanie nicka i czasu

--- public/msgsubmit.js
+++ public/msgsubmit.js
@@ -1,10 +1,11 @@
 var msgsubmit = angular.module('msgsubmit', []).controller('chatcore', ['$scope', function ($scope) {
     $scope.readyToSend = {};
     $scope.send = function () {
+        $scope.core.date = firebase.database.ServerValue.TIMESTAMP;
         var fb = firebase.database();
         var key = firebase.database().ref().child('posts').push().key;
         var updates = {};
-        updates['/posts/' + key] = $scope.core;
+        updates['/writes/' + key] = $scope.core;
      //   updates['/user-posts/' + uid + '/' + key] = postData;
         firebase.database().ref().update(updates);
     };
